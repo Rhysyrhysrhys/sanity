7b180211ead9cc7df7c166d6e7f6012ce4ce6820
Rhys Davies <rhys.davies901@gmail.com>
2016-12-17 12:39:56 -0800

    Adding in the beginnings of a World

--- src/main/java/com/modrons/map/Tile.java
+++ src/main/java/com/modrons/map/Tile.java
@@ -1,8 +1,30 @@
 package com.modrons.map;
 
+import java.awt.Color;
+
+import asciiPanel.AsciiPanel;
+
 /**
  * Created by rhys on 12/17/16.
  */
 public enum Tile {
-    
+    FLOOR((char) 250, AsciiPanel.yellow),
+    WALL((char) 177, AsciiPanel.yellow),
+    BOUND('x', AsciiPanel.brightBlack);
+
+    private final char glyph;
+    private final Color color;
+
+    Tile(char glyph, Color color) {
+        this.glyph = glyph;
+        this.color = color;
+    }
+
+    public char getGlyph() {
+        return glyph;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
